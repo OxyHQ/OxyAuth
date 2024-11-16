f1c874db32ae0f2ab76687610f0b3d1a0638b071
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2024-11-17 00:02:20 +0100

    Update site configuration with new branding and support links

--- config/site.ts
+++ config/site.ts
@@ -4,16 +4,16 @@ import { env } from "@/env.mjs";
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-  name: "Next Starter",
+  name: "Oxy Auth",
   description:
     "Get your project off to an explosive start with Auth & User Roles! Harness the power of Next.js 14, Prisma, Neon, Auth.js v5, Resend, React Email, Shadcn/ui to build your next big thing.",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
-    twitter: "https://twitter.com/miickasmt",
-    github: "https://github.com/mickasmt/next-auth-roles-template",
+    twitter: "https://twitter.com/Oxy_HQ",
+    github: "https://github.com/OxyHQ",
   },
-  mailSupport: "support@next-starter.fake",
+  mailSupport: "support@oxy.so",
 };
 
 export const footerLinks: SidebarNavItem[] = [
