4232eee81157f35524266a01ac86504f42e51e06
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2024-11-23 06:31:09 +0100

    Implement OAuth API endpoints and remove deprecated routes

--- app/api/oauth/client/route.ts
+++
@@ -1,8 +0,0 @@
-import OAuthServer from "@/server/oauth-server";
-
-export async function POST(request: Request) {
-  const oauth = OAuthServer;
-  const response = await oauth.authorize(request, new Response());
-
-  return response;
-}
