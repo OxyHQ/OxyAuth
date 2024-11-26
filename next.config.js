e8fa0d0bbc348c399d609761e0d99001dc8beb86
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2024-11-26 15:57:50 +0100

    Add CORS headers configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -28,3 +28,31 @@ const nextConfig = {
 };
 
 module.exports = withContentlayer(nextConfig);
+
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://auth.oxy.so https://mention.earth http://localhost:3000",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
